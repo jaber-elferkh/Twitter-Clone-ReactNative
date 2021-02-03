34e8b132125bec75eaead19a60c82d541ce13593
jaeel29 <elferkhjaber@gmail.com>
2021-02-03 18:36:14 +0100

    last commit

--- screens/NewTweetScreen.tsx
+++ screens/NewTweetScreen.tsx
@@ -1,5 +1,5 @@
 import { AntDesign } from '@expo/vector-icons';
-import * as React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -8,8 +8,11 @@ import { View } from '../components/Themed';
 import Colors from '../constants/Colors';
 
 export default function NewTweetScreen() {
+  const [tweet, setTweet] = useState('');
+  const [image, setImage] = useState('');
+
   const onPressTweet = () => {
-    console.log('onPress Tweet');
+    console.log(`Posting the tweet: ${tweet} and the image: ${image}`);
   };
 
   return (
@@ -20,15 +23,26 @@ export default function NewTweetScreen() {
           <Text style={styles.buttonText}>Tweet</Text>
         </TouchableOpacity>
       </View>
-      <View>
+      <View style={styles.newTweetContainer}>
         <ProfilePicture
           image={
             'https://images.unsplash.com/photo-1515121240746-1fc801ba75a8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80'
           }
         />
         <View style={styles.inputsContainer}>
-          <TextInput style={styles.tweetInput} placeholder="What's hapening" />
           <TextInput
+            value={tweet}
+            onChangeText={(e) => setTweet(e)}
+            multiline={true}
+            numberOfLines={3}
+            style={styles.tweetInput}
+            placeholder="What's hapening"
+          />
+          <TextInput
+            value={image}
+            onChangeText={(e) => setImage(e)}
+            multiline={true}
+            numberOfLines={3}
             style={styles.imageInput}
             placeholder="Image Url (optional)"
           />
@@ -41,7 +55,7 @@ export default function NewTweetScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: 'flex-start',
     backgroundColor: 'white',
   },
   headerContainer: {
@@ -62,7 +76,21 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 16,
   },
-  inputsContainer: {},
-  tweetInput: {},
-  imageInput: {},
+  newTweetContainer: {
+    flexDirection: 'row',
+    padding: 15,
+  },
+  inputsContainer: {
+    paddingLeft: 15,
+  },
+  tweetInput: {
+    maxWidth: 300,
+    // borderWidth: 2,
+    fontSize: 18,
+  },
+  imageInput: {
+    maxWidth: 300,
+    // borderWidth: 2,
+    fontSize: 18,
+  },
 });
